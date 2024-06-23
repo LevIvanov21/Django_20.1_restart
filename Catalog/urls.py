4f67afb2273849e1b3a3a57089c9e44e85376fd2
Лев Иванов <leevivanov@mail.ru>
2024-06-23 23:40:47 +0300

    urls.py

--- Catalog/urls.py
+++ Catalog/urls.py
@@ -1,10 +1,10 @@
 from django.conf import settings
 from django.urls import path
 from Catalog.apps import CatalogConfig
-from Catalog.views import index, contacts
+from Catalog.views import  product_list
 from django.conf.urls.static import static
 
 
 app_name = CatalogConfig.name
 
-urlpatterns = [path("", index), path("contacts/", contacts)] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+urlpatterns = [path("", product_list, name='product_list')] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
