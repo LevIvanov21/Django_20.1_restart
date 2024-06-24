8501332270f0a48df52c4c02bb2d0d64cd6028c9
Лев Иванов <leevivanov@mail.ru>
2024-06-25 00:59:23 +0300

    urls.py

--- config/urls.py
+++ config/urls.py
@@ -4,5 +4,6 @@ from django.contrib import admin
 from django.urls import path, include
 
 urlpatterns = [path("admin/", admin.site.urls),
-               path("", include("Catalog.urls", namespace='Catalog')),
+               path("", include("Catalog.urls")),
+               path("BLOG/", include("BLOG.urls", namespace="BLOG")),
                ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
