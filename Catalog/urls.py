1eb4accba7a091ae2502acfce1e60d469e0d556a
Лев Иванов <leevivanov@mail.ru>
2024-06-25 00:08:39 +0300

    urls.py

--- Catalog/urls.py
+++ Catalog/urls.py
@@ -1,10 +1,19 @@
 from django.conf import settings
 from django.urls import path
 from Catalog.apps import CatalogConfig
-from Catalog.views import  product_list
+from Catalog.views import base, ProductListView, ProductDetailView, contacts, ProductCreateView, ProductUpdateView, \
+    ProductDeleteView
 from django.conf.urls.static import static
+from django.urls import path
 
+# from myapp.views import HomePageView
 
 app_name = CatalogConfig.name
 
-urlpatterns = [path("", product_list, name='product_list')] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+urlpatterns = [path("", ProductListView.as_view(), name='product_list'),
+               path("<int:pk>/", ProductDetailView.as_view(), name='product_detail'),
+               path('contacts/', contacts, name='contacts'),
+               path('catalog/create', ProductCreateView.as_view(), name='product_create'),
+               path('<int:pk>/update/', ProductUpdateView.as_view(), name='product_update'),
+               path('<int:pk>/delete/', ProductDeleteView.as_view(), name='product_delete')
+               ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
