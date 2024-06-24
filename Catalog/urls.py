ff3a2a6264a633a1774596c16c91308db96210f0
Лев Иванов <leevivanov@mail.ru>
2024-06-25 00:45:04 +0300

    urls.py

--- Catalog/urls.py
+++ Catalog/urls.py
@@ -13,7 +13,7 @@ app_name = CatalogConfig.name
 urlpatterns = [path("", ProductListView.as_view(), name='product_list'),
                path("<int:pk>/", ProductDetailView.as_view(), name='product_detail'),
                path('contacts/', contacts, name='contacts'),
-               path('catalog/create', ProductCreateView.as_view(), name='product_create'),
+               path('product/create', ProductCreateView.as_view(), name='product_create'),
                path('<int:pk>/update/', ProductUpdateView.as_view(), name='product_update'),
                path('<int:pk>/delete/', ProductDeleteView.as_view(), name='product_delete')
                ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
