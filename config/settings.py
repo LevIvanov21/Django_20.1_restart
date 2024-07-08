1a0e2880a06d61d8d47035e1c0cab709914f0828
Лев Иванов <leevivanov@mail.ru>
2024-07-08 23:48:53 +0300

    settings.py

--- config/settings.py
+++ config/settings.py
@@ -1,3 +1,4 @@
+import os.path
 from pathlib import Path
 
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -16,6 +17,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'Catalog',
+    'BLOG',
 ]
 
 MIDDLEWARE = [
@@ -87,6 +89,6 @@ STATIC_URL = "static/"
 STATICFILES_DIRS = [BASE_DIR / 'static',]
 
 MEDIA_URL = 'media/'
-MEDIA_ROOT = (BASE_DIR, 'media')
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
